0185e85575db75c269bbf65b091879744fddf617
MatheusMaca <macariniking@gmail.com>
2018-06-08 23:38:12 -0300

    Core

---
+++ Modelos/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+
+        [Required] // Campo obrigatorio
+        [StringLength(30)] // tamanho maximo 30
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
